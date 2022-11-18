ed53a7efc32b69340acae3f2688b8fa0b0e2e7c8
lucasfemar <lucasdeveloper98l@gmail.com>
2022-11-17 21:08:34 -0300

    Banco de dados 2.0 Mais criaçao de ususario (Sem verificação)

--- backend/src/Controller/UserController.js
+++ backend/src/Controller/UserController.js
@@ -1,29 +1,61 @@
 const UserModel = require("../Model/UserModel")
 
-UserModel
 const UserController = {
-   create(request, response) {
+   async create(request, response) {
       const { nome, cpf, email, senha, dataNascimento } = request.body
+      try {
+         if (!nome) {
+            throw new Error("Preencha o nome")
+         }
 
-      if(!nome){
+         const ExistUser = await UserModel.getByEmail(email);
+
+         if(ExistUser){
+            throw new Error("Já existe um usuário cadastrado com esse email!")
+         }
+
+         const user = {
+            nome, cpf, email, senha, dataNascimento
+         }
+
+         await UserModel.createUser(user)
+
+         response.status(201).json({
+            "status": "sucesso",
+            "message": "Usuário criado com sucesso!"
+         });
+      } catch (error) {
          response.status(500).json({
             "status": "failure",
-            "message": "Preencha o nome!"
+            "message": `${error}`
          })
       }
-
-      const user = {
-         nome, cpf, email, senha, dataNascimento
-      }
-
-      UserModel.createUser(user)
-
-      response.status(201).send();
    },
 
-   getAll(request, response){
-      const users = UserModel.getAll();
-      response.json(users)
+   async getAll(request, response) {
+      try {
+          const users = await UserModel.getAll();
+         console.log(users)
+         response.json(users)
+      } catch (error) {
+         response.status(500).json({
+            "status": "failure",
+            "message": error
+         })
+      }
+   },
+
+   async getById(request, response) {
+      try {
+         const userId = request.params.id
+         const user = await UserModel.getById(userId);
+         response.json(user)
+      } catch (error) {
+         response.status(500).json({
+            "status": "failure",
+            "message": error
+         })
+      }
    }
 }
 
--- backend/src/Model/UserModel.js
+++ backend/src/Model/UserModel.js
@@ -1,11 +1,33 @@
-const Users = [];
+const Database = require("../db/config")
+
 const UserModel = {
-   getAll() {
-      return Users;
+   async getAll() {
+      const db = await Database();
+      const users = await db.all(`SELECT * FROM User`)
+      return users;
    },
 
-   createUser(user) {
-      Users.push(user)
+   async getById(userId) {
+      const db = await Database();
+      const user = await db.get(`SELECT * FROM User WHERE id = ${userId}`)
+      return user;
+   },
+
+   async getByEmail(userEmail) {
+      const db = await Database();
+      const user = await db.get(`SELECT * FROM User WHERE email = '${userEmail}'`);
+      return user;
+   },
+
+   async createUser(user) {
+      const db = await Database();
+      await db.run(`
+         INSERT INTO User 
+         (nome, cpf, email, senha, dataNascimento)
+         VALUES ('${user.nome}', '${user.cpf}', '${user.email}', '${user.name}', '${user.dataNascimento}')
+      `);
+
+      db.close()
    }
 };
 
