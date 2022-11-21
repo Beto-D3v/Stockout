b465c075bc561591a5fc018ecf628dfe64ab2fa5
Beto-D3v <96922328+Beto-D3v@users.noreply.github.com>
2022-11-21 16:41:41 -0300

    Update index.js

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -5,7 +5,10 @@ import SignIn from '../pages/SignIn'
 import SignInCompany from '../pages/SignInCompany'
 import ClientRegister from '../pages/ClientRegister'
 import CompanyRegister from '../pages/CompanyRegister'
-
+import Menu from '../pages/Menu'
+import AbaMercado from '../pages/AbaMercado'
+import AbaProdutos from '../pages/AbaProdutos'
+import Carrinho from '../pages/Carrinho'
 
 
 const Stack = createNativeStackNavigator();
@@ -42,4 +45,30 @@ export default function Routes(){
             component = {SignInCompany}
             options={{headerShown:false}}
         />
+
+        <Stack.Screen
+            name="Menu"
+            component = {Menu}
+            options={{headerShown:false}}
+        />
+
+        <Stack.Screen
+            name="AbaMercado"
+            component = {AbaMercado}
+            options={{headerShown:false}}
+        />
+
+        <Stack.Screen
+            name="AbaProdutos"
+            component = {AbaProdutos}
+            options={{headerShown:false}}
+        />
+
+        <Stack.Screen
+            name="Carrinho"
+            component = {Carrinho}
+            options={{headerShown:false}}
+        />
+
+
      </Stack.Navigator> )}
